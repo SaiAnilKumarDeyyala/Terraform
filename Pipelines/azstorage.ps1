ed3a5b446438d9bc1b650838ff86f6902ada751b
SaiAnilKumarDeyyala <darlingss014@gmail.com>
2023-03-18 17:29:34 +0530

    delete a folder

--- Pipelines/azstorage.ps1
+++
@@ -1,9 +0,0 @@
-az storage account create --name $(terraformstorageaccount) --resource-group $(terraformstoragerg) --location eastus --sku Standard_LRS
-
-az storage container create --name tfstate --account-name $(terraformstorageaccount)
-
-az storage account keys list -g $(terraformstoragerg) -n $(terraformstorageaccount)
-
-$key=(Get-AzureRmStorageAccountKey -ResourceGroupName $(terraformstoragerg) -AccountName $(terraformstorageaccount)).Value[0]
-
-Write-host "##vso[task.setvariable variable=storagekey]$key"
