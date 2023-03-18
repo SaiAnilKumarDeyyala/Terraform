fb49ac7cd1a546420750e8b647374935b47d402e
SaiAnilKumarDeyyala <darlingss014@gmail.com>
2023-03-18 17:46:47 +0530

    minor change

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "storage_account" {
 module "virtual_network" {
     source                  = "./Modules/virtual_network"
     vnet_name               = var.vnet_name
+    resource_group_name     = var.resource_group_name
     nsg_name                = var.nsg_name
     subnet_name             = var.subnet_name
     subnettwo_name          = var.subnettwo_name
